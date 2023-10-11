b7c7e62d9520285c66dd0cb939a00e1c73b7f2aa
Emmanuel Jemeni <jemenichinonso11@gmail.com>
2023-10-11 16:35:50 +0100

    feat: Switched from npm to bun. Switched from App Router to Pages Router

---
+++ features.ts
@@ -0,0 +1,52 @@
+const features: { id: number; feature: string; details: string }[] = [
+  {
+    id: 0,
+    feature: "User Registration and Login",
+    details:
+      "Allow users to create accounts and log in securely to access their personal journal.",
+  },
+  {
+    id: 1,
+    feature: "Create Journal Entries",
+    details:
+      "Provide a user-friendly interface for users to create new journal entries. Include text fields for writing about daily experiences, thoughts, and reflections.",
+  },
+  {
+    id: 2,
+    feature: "Tagging and Categorization",
+    details:
+      "Allow users to tag their journal entries with keywords or create categories to organize and find entries easily. This helps in structuring and searching for specific content.",
+  },
+  {
+    id: 3,
+    feature: "Date and Time Stamps",
+    details:
+      "Automatically capture and display the date and time when entries are created, providing a chronological order of journal entries.",
+  },
+  {
+    id: 4,
+    feature: "Search Functionality",
+    details:
+      "Implement a search feature that enables users to search for specific keywords, tags, or phrases within their journal entries, making it easier to find relevant content.",
+  },
+  {
+    id: 5,
+    feature: "Editing and Updating Entries",
+    details:
+      "Allow users to edit and update their existing journal entries, giving them the flexibility to refine their thoughts or add new information.",
+  },
+  {
+    id: 6,
+    feature: "Statistics and Insights",
+    details:
+      "Generate statistics and insights based on user activity, such as the number of entries created, word count trends, or mood distribution. Provide visualizations or summary reports to help users gain insights into their journaling habits and patterns.",
+  },
+  {
+    id: 7,
+    feature: "Export and Backup",
+    details:
+      "Offer the ability to export journal entries or backup the entire journal to external storage or cloud services, ensuring data preservation and enabling users to access their journal across devices.",
+  },
+];
+
+export default features;
